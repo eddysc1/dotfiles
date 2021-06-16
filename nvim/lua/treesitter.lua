8a1f452995f96be0c72a5646891680795d64e8f7
Edvardas Gaidelis <edvardassc@gmail.com>
2021-06-16 23:10:32 +0300

    dotfiles commit

--- nvim/lua/treesitter.lua
+++ nvim/lua/treesitter.lua
@@ -1,4 +1,8 @@
 require'nvim-treesitter.configs'.setup {
   ensure_installed = { "javascript", "typescript", "lua", "html", "php", "scss" },
   highlight = { enable = true },
+  indent = {
+    enable = true,
+    disable = { "javascript", "typescript", "lua", "html", "scss" },
+  },
 }
